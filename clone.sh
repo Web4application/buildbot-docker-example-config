625cb0c1de45002ad5438ca718d6479bdc118179
Seriki yakub <sandraisom00@gmail.com>
2026-01-02 23:44:06 +0100

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -4,3 +4,8 @@ $ git clone --depth 1 https://github.com/buildbot/buildbot-docker-example-config
 # Build the Buildbot container (it will take a few minutes to download packages)
 cd buildbot-docker-example-config/simple
 docker-compose up
+
+git branch -m master Main
+git fetch origin
+git branch -u origin/Main Main
+git remote set-head origin -a
