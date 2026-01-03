c1d4577f9bf229d7bccf5cb3992cf0288ce06283
Seriki yakub <sandraisom00@gmail.com>
2026-01-03 12:05:09 +0100

    Update and rename bash.sh to pnpm -r build bash.sh

--- pnpm -r build bash.sh
+++ pnpm -r build bash.sh
@@ -1,3 +1,8 @@
 npm install @huggingface/inference
 npm install @huggingface/hub
 npm install @huggingface/mcp-client
+sudo corepack enable
+pnpm install
+pnpm -r format:check
+pnpm -r lint:check
+pnpm -r test
